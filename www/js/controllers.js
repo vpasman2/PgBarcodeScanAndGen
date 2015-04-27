b049af0ce1a6bca87d83de2e25a0946a636d8977
Vladimir Pasman <vpasman2@gmail.com>
2015-04-27 18:38:14 -0500

    initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,67 @@
+angular.module('starter.controllers', [])
+
+.controller('GenerateCtrl', function($scope, appServices) {
+    $scope.message = '';
+    $scope.click = function() {
+        var promise = appServices.generateBarcode();
+        promise.then(
+            function(result) {
+                if (result.error == false) {
+                    //$scope.message = '<div id="qrcode">'+result.result+'</div>';
+                }
+                else {
+                    $scope.message = '<b>ERROR</b>: ' + result;
+                }
+            },
+            function(result) {
+                $scope.message = '' + result.error;
+            },
+            function(result) {
+                $scope.message = '' + result.error;
+            });
+    }
+
+    $scope.clear = function() {
+        $scope.message = '';
+    }
+
+    $scope.click();
+})
+
+.controller('ScanCtrl', function($scope, appServices) {
+    $scope.message = '';
+    $scope.click = function() {
+        var promise = appServices.scanBarcode();
+        promise.then(
+            function(result) {
+                if (result.error == false) {
+                    var d = new Date();
+                    $scope.message = '<table>' +
+                        '<tbody>' +
+                        '<tr><td>Timestamp:</td><td>&nbsp;</td><td>' + d.toUTCString() + '</td></tr>' +
+                        '<tr><td>Text:</td><td>&nbsp;</td><td>' + result.result.text + '</td></tr>' +
+                        '<tr><td>Format:</td><td>&nbsp;</td><td>' + result.result.format + '</td></tr>' +
+                        '<tr><td>Text:</td><td>&nbsp;</td><td>' + result.result.cancelled + '</td></tr>' +
+                        '</tbody>' +
+                        '</table>';
+                }
+                else {
+                    $scope.message = '<b>ERROR</b>: ' + result;
+                }
+            },
+            function(result) {
+                $scope.message = '' + result.error;
+            },
+            function(result) {
+                $scope.message = '' + result.error;
+            });
+    }
+
+    $scope.clear = function() {
+        $scope.message = '';
+    }
+})
+
+.controller('AboutCtrl', function($scope) {
+})
+
